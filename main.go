8324a248880fb4788301a21341e999e95cef6f4b
https://github.com/reyhardy <rezahadzri@gmail.com>
2024-06-29 00:15:20 +0800

    add insert and select query using table model and qb

--- main.go
+++ main.go
@@ -6,35 +6,31 @@ import (
 
 	"github.com/gocql/gocql"
 	"github.com/scylladb/gocqlx/v2"
+	"github.com/scylladb/gocqlx/v2/qb"
 	"github.com/scylladb/gocqlx/v2/table"
+)
+
+const (
+	keyspace  = "testing"
+	tableName = "persons"
 )
 
 func main() {
 	cluster := gocql.NewCluster("172.17.0.2")
-	cluster.Keyspace = "testing"
-	cluster.ProtoVersion = 4
 
 	session, err := gocqlx.WrapSession(cluster.CreateSession())
 	if err != nil {
 		log.Fatal("error connect db: ", err)
 	}
+
 	defer session.Close()
 
-	err = session.ExecStmt(fmt.Sprintf("CREATE KEYSPACE IF NOT EXISTS %v WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 3};", cluster.Keyspace))
+	session.ExecStmt(fmt.Sprintf("DROP KEYSPACE IF EXISTS %s;", keyspace))
+
+	err = session.ExecStmt(fmt.Sprintf("CREATE KEYSPACE IF NOT EXISTS %s WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1};", keyspace))
 	if err != nil {
 		log.Fatal("error create keyspace: ", err)
 	}
-
-	var personMetadata = table.Metadata{
-		Name:    "person",
-		Columns: []string{"first_name", "last_name", "age"},
-		PartKey: []string{"first_name"},
-		SortKey: []string{"last_name"},
-	}
-
-	log.Printf("person table metadata: %#v", personMetadata)
-
-	var personTable = table.New(personMetadata)
 
 	type Person struct {
 		FirstName string
@@ -42,21 +38,53 @@ func main() {
 		Age       int
 	}
 
-	p := Person{
-		"AlJuan",
-		"Hardy",
-		34,
+	err = session.ExecStmt(fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s.%s(
+		first_name text PRIMARY KEY,
+		last_name text,
+		age int
+	);`, keyspace, tableName))
+	if err != nil {
+		log.Fatal("error create table: ", err)
 	}
 
-	iq := session.Query(personTable.Insert()).BindStruct(p)
-	if err := iq.ExecRelease(); err != nil {
-		log.Fatal("error insert table: ", err)
+	personMetadata := table.Metadata{
+		Name:    fmt.Sprintf("%s.%s", keyspace, tableName),
+		Columns: []string{"first_name", "last_name", "age"},
+		PartKey: []string{"first_name"},
 	}
 
-	gq := session.Query(personTable.Get()).BindStruct(p)
-	if err := gq.GetRelease(&p); err != nil {
-		log.Fatal("error get table: ", err)
+	//insert using query builder
+	insertQuery := qb.Insert(personMetadata.Name).Columns(personMetadata.Columns...).Query(session)
+	insertQuery.BindStruct(Person{
+		FirstName: "reza",
+		LastName:  "hadzri",
+		Age:       34,
+	})
+	if err := insertQuery.ExecRelease(); err != nil {
+		log.Fatal("insertExecRelease() failed: ", err)
 	}
 
-	log.Printf("person table: %#v", personTable)
+	//insert using table model
+	personTable := table.New(personMetadata)
+	insertPerson := personTable.InsertQuery(session)
+	insertPerson.BindStruct(&Person{
+		FirstName: "juan",
+		LastName:  "hardy",
+		Age:       34,
+	})
+
+	//select using table model
+	queryPerson := personTable.SelectQuery(session)
+	queryPerson.BindStruct(&Person{
+		FirstName: "reza",
+	})
+
+	var person []*Person
+	if err := queryPerson.Select(&person); err != nil {
+		log.Fatal("Select() failed: ", err)
+	}
+
+	for _, i := range person {
+		log.Printf("Select people: %#v", *i)
+	}
 }
